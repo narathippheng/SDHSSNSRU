4725baacff0df6c639b90341c4a1515a9b484d8c
narathippheng <67501003+narathippheng@users.noreply.github.com>
2024-07-20 11:02:01 +0700

    Update

--- layers/layers.js
+++ layers/layers.js
@@ -23,16 +23,70 @@ var lyr__1 = new ol.layer.Vector({
                 declutter: false,
                 source:jsonSource__1, 
                 style: style__1,
+                popuplayertitle: "มิติด้านเศรษฐกิจ",
+                interactive: true,
+                title: '<img src="styles/legend/_1.png" /> มิติด้านเศรษฐกิจ'
+            });
+var format__2 = new ol.format.GeoJSON();
+var features__2 = format__2.readFeatures(json__2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__2.addFeatures(features__2);
+var lyr__2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__2, 
+                style: style__2,
+                popuplayertitle: "มิติด้านสังคม",
+                interactive: true,
+                title: '<img src="styles/legend/_2.png" /> มิติด้านสังคม'
+            });
+var format__3 = new ol.format.GeoJSON();
+var features__3 = format__3.readFeatures(json__3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__3.addFeatures(features__3);
+var lyr__3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__3, 
+                style: style__3,
+                popuplayertitle: "มิติด้านสิ่งแวดล้อม",
+                interactive: true,
+                title: '<img src="styles/legend/_3.png" /> มิติด้านสิ่งแวดล้อม'
+            });
+var format__4 = new ol.format.GeoJSON();
+var features__4 = format__4.readFeatures(json__4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__4.addFeatures(features__4);
+var lyr__4 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__4, 
+                style: style__4,
                 popuplayertitle: "ตำบลนครสวรรค์ออก",
                 interactive: true,
-                title: '<img src="styles/legend/_1.png" /> ตำบลนครสวรรค์ออก'
+                title: '<img src="styles/legend/_4.png" /> ตำบลนครสวรรค์ออก'
             });
 
-lyr_OpenStreetMap_0.setVisible(true);lyr__1.setVisible(true);
-var layersList = [lyr_OpenStreetMap_0,lyr__1];
-lyr__1.set('fieldAliases', {'AP_TN': 'AP_TN', 'PV_TN': 'PV_TN', 'MOO': 'MOO', 'TB_TN': 'TB_TN', });
-lyr__1.set('fieldImages', {'AP_TN': 'TextEdit', 'PV_TN': 'TextEdit', 'MOO': 'TextEdit', 'TB_TN': 'TextEdit', });
-lyr__1.set('fieldLabels', {'AP_TN': 'no label', 'PV_TN': 'no label', 'MOO': 'inline label - always visible', 'TB_TN': 'no label', });
-lyr__1.on('precompose', function(evt) {
+lyr_OpenStreetMap_0.setVisible(true);lyr__1.setVisible(true);lyr__2.setVisible(true);lyr__3.setVisible(true);lyr__4.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr__1,lyr__2,lyr__3,lyr__4];
+lyr__1.set('fieldAliases', {'Id': 'Id', 'Moo': 'Moo', 'type of so': 'type of so', 'detail': 'detail', 'Des': 'Des', 'y': 'y', 'x': 'x', 'Image': 'Image', 'type of ec': 'type of ec', 'layer': 'layer', 'path': 'path', });
+lyr__2.set('fieldAliases', {'Id': 'Id', 'Moo': 'Moo', 'type of so': 'type of so', 'detail': 'detail', 'Des': 'Des', 'y': 'y', 'x': 'x', 'Image': 'Image', 'typy of so': 'typy of so', 'das': 'das', 'layer': 'layer', 'path': 'path', });
+lyr__3.set('fieldAliases', {'Id': 'Id', 'Moo': 'Moo', 'Type of en': 'Type of en', 'Detail': 'Detail', 'Des': 'Des', 'y': 'y', 'x': 'x', 'Image': 'Image', 'type of ec': 'type of ec', 'type of  e': 'type of  e', 'layer': 'layer', 'path': 'path', });
+lyr__4.set('fieldAliases', {'AP_TN': 'AP_TN', 'PV_TN': 'PV_TN', 'MOO': 'MOO', 'TB_TN': 'TB_TN', });
+lyr__1.set('fieldImages', {'Id': 'TextEdit', 'Moo': 'TextEdit', 'type of so': 'TextEdit', 'detail': 'TextEdit', 'Des': 'TextEdit', 'y': 'TextEdit', 'x': 'TextEdit', 'Image': 'TextEdit', 'type of ec': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', });
+lyr__2.set('fieldImages', {'Id': 'TextEdit', 'Moo': 'TextEdit', 'type of so': 'TextEdit', 'detail': 'TextEdit', 'Des': 'TextEdit', 'y': 'TextEdit', 'x': 'TextEdit', 'Image': 'TextEdit', 'typy of so': 'TextEdit', 'das': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', });
+lyr__3.set('fieldImages', {'Id': 'TextEdit', 'Moo': 'TextEdit', 'Type of en': 'TextEdit', 'Detail': 'TextEdit', 'Des': 'TextEdit', 'y': 'TextEdit', 'x': 'TextEdit', 'Image': 'TextEdit', 'type of ec': 'TextEdit', 'type of  e': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', });
+lyr__4.set('fieldImages', {'AP_TN': 'TextEdit', 'PV_TN': 'TextEdit', 'MOO': 'TextEdit', 'TB_TN': 'TextEdit', });
+lyr__1.set('fieldLabels', {'Id': 'no label', 'Moo': 'no label', 'type of so': 'no label', 'detail': 'no label', 'Des': 'no label', 'y': 'no label', 'x': 'no label', 'Image': 'no label', 'type of ec': 'no label', 'layer': 'no label', 'path': 'no label', });
+lyr__2.set('fieldLabels', {'Id': 'no label', 'Moo': 'no label', 'type of so': 'no label', 'detail': 'no label', 'Des': 'no label', 'y': 'no label', 'x': 'no label', 'Image': 'no label', 'typy of so': 'no label', 'das': 'no label', 'layer': 'no label', 'path': 'no label', });
+lyr__3.set('fieldLabels', {'Id': 'no label', 'Moo': 'no label', 'Type of en': 'no label', 'Detail': 'no label', 'Des': 'no label', 'y': 'no label', 'x': 'no label', 'Image': 'no label', 'type of ec': 'no label', 'type of  e': 'no label', 'layer': 'no label', 'path': 'no label', });
+lyr__4.set('fieldLabels', {'AP_TN': 'no label', 'PV_TN': 'no label', 'MOO': 'no label', 'TB_TN': 'no label', });
+lyr__4.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- styles/_4_style.js
+++ styles/_4_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style__1 = function(feature, resolution){
+var style__4 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -9,20 +9,19 @@ var style__1 = function(feature, resolution){
     var value = ""
     var labelText = "";
     size = 0;
-    var labelFont = "10px, sans-serif";
-    var labelFill = "#000000";
+    var labelFont = "19.5px \'Angsana New\', sans-serif";
+    var labelFill = "#323232";
     var bufferColor = "";
     var bufferWidth = 0;
     var textAlign = "left";
     var offsetX = 8;
     var offsetY = 3;
     var placement = 'point';
-    if ("" !== null) {
-        labelText = String("");
+    if (feature.get("MOO") !== null) {
+        labelText = String(feature.get("MOO"));
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 4.0 + size,
-            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(219,30,42,1.0)'})}),
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: [2.508,5.016], lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
